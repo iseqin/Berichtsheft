5d3009a728f1adb49c383f2c5f0c6a2077b7eb88
Sebastian Schmidt <iseqkin@gmail.com>
2018-03-01 18:18:34 +0100

    First label, input, button

---
+++ js/renderer.js
@@ -0,0 +1,9 @@
+function showDate(){
+    try{
+        var dateField = document.getElementById("i_eingabe").value;
+        console.log(dateField);
+    }
+    catch(e){
+        console.log(e);
+    }
+}
